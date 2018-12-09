9944e6054b9fcc2abc3fddc0e7c9d1aa6a2f34bb
Guillermo García López <memowii@hotmail.com>
2018-12-09 17:02:32 -0600

    add tickets to database

--- renderer.js
+++ renderer.js
@@ -1,5 +1,136 @@
+// https://codeburst.io/node-js-mysql-and-promises-4c3be599909b 
+
 let app = require('electron').remote;
 let fs = require('fs');
+let mysql = require('mysql');
+
+class DB {
+  constructor() {
+    this.connection = new mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: 'gui_tickets'
+    });
+  }
+
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err)
+          return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err)
+          return reject(err);
+        resolve();
+      });
+    });
+  }
+
+  fetchTickets() {
+    return new Promise((resolve, reject) => {
+      this.connection.query('SELECT * FROM tickets', (error, results) => {
+        if (error) throw reject(error);
+        resolve(results);
+      });
+    });
+  }
+
+  insertTicket(ticket) {
+    return new Promise((resolve, reject) => {
+      this.connection.query('INSERT INTO tickets SET ?', ticket, (error, result) => {
+        if (error) throw reject(error); 
+        resolve(result); 
+      });
+    });
+  }
+
+  insertTickets(tickets) {
+    return new Promise((resolve, reject) => {
+      this.connection.query('INSERT INTO tickets (consecutivo, esta_usado) VALUES ?', [tickets], (error, result) => {
+        if (error) throw reject(error); 
+        resolve(result); 
+      });
+    });
+  }
+
+  fetchTicket(id) {
+    return new Promise((resolve, reject) => {
+      this.connection.query('SELECT * FROM tickets WHERE id=?', [id], (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+    });
+  }
+
+  updateTicket(ticket) {
+    return new Promise((resolve, reject) => {
+      this.connection.query('UPDATE tickets SET consecutivo=?, esta_usado=? WHERE id=?', 
+      [ticket.consecutivo, ticket.esta_usado, ticket.id], (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+    });
+  }
+}
+
+DB.execute = function(callback) {
+  const db = new DB();
+  return callback(db).then(
+      result => db.close().then(() => result),
+      error => db.close().then(() => {throw error;})
+  );
+}
+
+class Table {
+  constructor(tableId) {
+    this.tableId = tableId;
+    this.table = document.getElementById(this.tableId);
+  }
+
+  addTickets(tickets) {
+    Object.entries(tickets).forEach(([key, value]) => {
+      let row = table.insertRow(1);
+      let numeroConsecutivoTicketCell = row.insertCell(0);
+      let accionesCell = row.insertCell(1);
+
+      numeroConsecutivoTicketCell.innerHTML = value;
+      if (areUsed) {
+        numeroConsecutivoTicketCell.style.textDecoration = 'line-through';
+      }
+      accionesCell.innerHTML = `<div>
+      <button class="copiar" type="button">Copiar</button>
+      <button class="marcar" type="button">Marcar</button>
+      </div>`;
+    });
+  }
+}
+
+function populateDB() {
+  let fileContent = fs.readFileSync('tickets-no-facturados-no-usados.txt', 'utf-8');
+  let consecutivos = fileContent.split('\n'); 
+  let tickets = [];
+
+  Object.entries(consecutivos).forEach(([key, consecutivo]) => {
+    // if used to avoid the "", but it didn't work.
+    if (!consecutivo) return;
+
+    let ticket = [consecutivo, false];
+    if (Number(ticket[0]) <= 89340) ticket[1] = true;
+    tickets.push(ticket);
+  });
+
+  DB.execute(db => db.insertTickets(tickets).then(result => {
+    console.log(result);
+  }));
+}
 
 function fillTable(tickets, areUsed = false) {
   let table = document.getElementById('table-numero-consecutivo-ticket');
@@ -17,7 +148,7 @@ function fillTable(tickets, areUsed = false) {
     <button class="copiar" type="button">Copiar</button>
     <button class="marcar" type="button">Marcar</button>
     </div>`;
-  }); 
+  });
 
   let copiarButtons = document.getElementsByClassName('copiar');
   Object.entries(copiarButtons).forEach(([key, copiarButton]) => {
@@ -29,47 +160,18 @@ function fillTable(tickets, areUsed = false) {
       document.execCommand('copy');
     });
   });
-  
+
   let marcarButtons = document.getElementsByClassName('marcar');
   Object.entries(marcarButtons).forEach(([key, marcarButton]) => {
-    marcarButton.addEventListener('click', function() {
+    marcarButton.addEventListener('click', function () {
       let tableData = this.parentElement.parentElement.parentElement.firstChild;
       tableData.style.textDecoration = 'line-through';
     });
   });
 }
 
-function readFileUsedTickets(filePath) {
-  fs.readFile(filePath, 'utf-8', function (err, data) {
-    if (err) {
-      throw "Error while opening file.";
-    }
+function main() {
 
-    addUsedTickets(data);
-  });
 }
 
-function readFileUnusedTickets(filePath) {
-  fs.readFile(filePath, 'utf-8', function (err, data) {
-    if (err) {
-      throw "Error while opening file.";
-    }
-
-    addUnusedTickets(data);
-  });
-}
-
-function addUsedTickets(readTicketsFromFile) {
-  let tickets = readTicketsFromFile.split('\n');
-  fillTable(tickets, true);
-}
-
-function addUnusedTickets(readTicketsFromFile) {
-  let tickets = readTicketsFromFile.split('\n');
-  fillTable(tickets, false);
-}
-
-
-
-readFileUnusedTickets('./tickets-no-facturados-no-usados.txt');
-readFileUsedTickets('./tickets-no-facturados-usados.txt');
+main();
